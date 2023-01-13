7340c88dd1cf84943c89aab8f188e3da8ab18deb
CharlesRado <charles.radolanirina@eleve.isep.fr>
2023-01-13 11:23:50 +0100

    Ajout de Bot7Wonder 2.0

--- src/main/java/com/isep/the7WondersArchitect/Bot7Wonder.java
+++ src/main/java/com/isep/the7WondersArchitect/Bot7Wonder.java
@@ -1,12 +1,21 @@
 package com.isep.the7WondersArchitect;
+import com.isep.items.cards.Card;
+import com.isep.items.cards.CardType;
+import com.isep.items.wonders.Wonders;
+
 import java.util.List;
 
 
 public class Bot7Wonder {
-    private GameState gameState;//état actuel du jeu qui possède une méthode 'play'
-
-    public Bot7Wonder (GameState gameState) {
-        this.gameState = gameState;
+    private Game gameState;//état actuel du jeu qui possède une méthode 'play'
+    private final String name;
+    private final int age;
+    private String civilisationName;
+    // Possession Player
+    private Wonders wonder;
+    public Bot7Wonder (String name, int age) {
+            this.name = name;
+            this.age = age;
     }
 
     public void play() {//méthode qui boucle jusqu'à la fin de la partie
@@ -23,6 +32,18 @@ public class Bot7Wonder {
     //ça dépend de notre stratégie et de nos objectifs pour le bot
     private Action chooseBestAction(List<Action> actions) {
 
+    }
+
+    public void readGame (List<Card> deckPlayer, List<Card> deckRightPlayer, List<Card> centraldeck){
+        this.gameState = Game.option;
+
+        System.out.println(deckRightPlayer.get(0).front.cardCategory);
+    }
+
+    private bestChoice(){
+
+
+        if (cartetype != CardType.valueOf("materrial"))
     }
 
     //Deep learning trop compliqué pour les jeux de plateau stratégique
